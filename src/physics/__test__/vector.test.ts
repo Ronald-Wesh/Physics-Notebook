02294f8725fb7eda9d4d187c93190eef314675a7
Ronald-Wesh <ronaldwaweru8@gmail.com>
2026-08-08 23:46:13 +0300

    test: add unit test for vector subtraction

--- src/physics/__test__/vector.test.ts
+++ src/physics/__test__/vector.test.ts
@@ -4,3 +4,6 @@ describe("Vec2", () => {
   it("adds component-wise", () => {
     expect(add(vec2(1, 2), vec2(3, 4))).toEqual({ x: 4, y: 6 });
   });
+    it("subtracts component-wise", () => {
+    expect(sub(vec2(3, 4), vec2(1, 2))).toEqual({ x: 2, y: 2 });
+  });
