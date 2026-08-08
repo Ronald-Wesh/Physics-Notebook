90be6b6f191b3832030d7623066f5d49f50f05c0
Ronald-Wesh <ronaldwaweru8@gmail.com>
2026-08-08 23:46:31 +0300

    test: add unit tests for vector scaling and length calculation

--- src/physics/__test__/vector.test.ts
+++ src/physics/__test__/vector.test.ts
@@ -7,3 +7,10 @@ describe("Vec2", () => {
     it("subtracts component-wise", () => {
     expect(sub(vec2(3, 4), vec2(1, 2))).toEqual({ x: 2, y: 2 });
   });
+ it("scales by a scalar", () => {
+    expect(scale(vec2(1, -2), 3)).toEqual({ x: 3, y: -6 });
+  });
+  it("calculates length correctly", () => {
+    expect(length(vec2(3, 4))).toBe(5);
+  });
+});
